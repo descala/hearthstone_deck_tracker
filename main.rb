72a9706cc22a48dfc7e27e0250699a51eca67b16
Lluís <lluis@ingent.net>
2015-11-29 22:52:12 +0100

    add options and config file

--- main.rb
+++ main.rb
@@ -1,22 +1,41 @@
 #!/usr/bin/env ruby
 require_relative 'lib/log_parser'
 require 'active_support/core_ext/hash/indifferent_access'
+require 'optparse'
+require 'json'
 
-LOG_DIR='/home/tictacbum/.local/share/wineprefixes/hearthstone/' +
-  'drive_c/Program Files/Hearthstone/Logs'
-LOG=[
-  "#{LOG_DIR}/Power.log",
-  "#{LOG_DIR}/Zone.log",
+if File.exist? 'config.json'
+  options = JSON.load(File.read('config.json')).with_indifferent_access
+end
+options ||= {}
+
+OptionParser.new do |opts|
+  opts.banner = "Usage: main.rb [options]"
+
+  opts.on("-p", "--log-path PATH", "Path to Hearthstone log dir") do |v|
+    options[:log_path] = v
+  end
+
+end.parse!
+
+log_files = [
+  "#{options[:log_path]}/Power.log",
+  "#{options[:log_path]}/Zone.log",
 ]
-puts "starting Hearthstone deck tracker"
+
+log_files.select {|f| !File.exist? f }.each do |f|
+  puts "Could not find log file, check log path #{f}"
+  exit 1
+end
 
 begin
+  puts "starting Hearthstone deck tracker"
+
   log_parser = LogParser.new(
-    log_file: LOG,
+    log_file: log_files,
   )
   log_parser.tail_f
 
 rescue Interrupt
-  puts
+  puts "bye!"
 end
-puts "bye!"
